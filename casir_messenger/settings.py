e8d5b97f959e83e9bfaf067b1411c8d8985a1aa6
Guillaume Chevallier <chevalgu@pc-a102-15.iut-valence.fr>
2014-04-17 14:31:19 +0200

    Some corrections cause of pull and declaration pb

--- casir_messenger/settings.py
+++ casir_messenger/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'friends',
     'user_manager',
     'debug_toolbar',
 )
--- user_manager/views.py
+++ user_manager/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponseRedirect
 
-
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 # Create your views here.
+
+
 def index(request):
+
+    if request.method == 'POST': # If the form has been submitted...
+        # ContactForm was defined in the the previous section
+        form = AuthenticationForm(data=request.POST) # A form bound to the POST data
+
+        if form.is_valid(): # All validation rules pass
+            # Process the data in form.cleaned_data
+            # ...
+            return HttpResponseRedirect('/thanks/') # Redirect after POST
+    else:
+        form = AuthenticationForm() # An unbound form
+
     context = {
-               "example_attribute": "Hello, world",
+               "form": form,
                }
     return render(request, "user_manager/layout.html", context)
-
-
-def newContact(request):
-    pass 
